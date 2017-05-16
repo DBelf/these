fc1838d4a37c2ca7d10f8a8665ea1464126362c8
DBelf <dimitri.belfor@gmail.com>
2017-05-16 18:21:31 +0200

    files should be read

--- ast_taint/app/generate_ast.js
+++ ast_taint/app/generate_ast.js
@@ -2,10 +2,23 @@
  * Created by dimitri on 16/05/2017.
  */
 var fs = require('fs'),
+    path = require('path'),
     esprima = require('esprima');
 
 var GenerateAST = (function (){
+    var _EXTENSION = '.js';
 
+    var collectFiles = function(path){
+        if (!fs.existsSync(path)){
+            // console.log("no dir ",path); //DEBUG
+            return;
+        }
+        
+    }
+
+    return {
+        collectFiles: collectFiles
+    }
 })();
 
 module.exports = GenerateAST;
--- ast_taint/test/test.js
+++ ast_taint/test/test.js
@@ -5,7 +5,8 @@ var chai = require('chai'),
     expect = chai.expect, // we are using the "expect" style of Chai
     sourceFind = require('../app/source_finder'),
     sinkFinder = require('../app/sink_finder'),
-    astCheck = require('../app/ast_manipulations');
+    astCheck = require('../app/ast_manipulations'),
+    generateAST = require('../app/generate_ast');
 
 var documentValue = JSON.parse(`{
     "type": "MemberExpression",
@@ -136,41 +137,55 @@ var messageManagerControl = JSON.parse(`{
             "kind": "let"
         }`);
 
-describe("Vulnerablility finder", function () {
-    describe("Document sources", function () {
-        it("finds the sources when doing a member call on documents", function () {
+describe('AST generation', function () {
+    describe('Collecting all javascript files', function () {
+        it('Can\'t find non-existing paths',function () {
+            var falsePath = ''
+            var value = generateAST.collectFiles(falsePath);
+            expect(value).to.be.undefined;
+        })
+        it('finds all files with the javascript extension', function () {
+
+        })
+    })
+
+})
+
+describe('Vulnerablility finder', function () {
+    describe('Document sources', function () {
+        it('finds the sources when doing a member call on documents', function () {
             var value = sourceFind.valueAccess(documentValue);
             expect(value).to.equal(true);
         });
-        it("finds a document source", function () {
+        it('finds a document source', function () {
             var value = sourceFind.generalCheck(documentMemberAccess);
             expect(value).to.equal(true);
         });
-        it("finds the value of a document element source", function () {
+        it('finds the value of a document element source', function () {
             var value = sourceFind.valueAccess(valueAccessNode);
             expect(value).to.equal(true);
         })
     });
-    describe("Potential communication sinks", function () {
-        it("checks whether a property exists", function () {
+    describe('Potential communication sinks', function () {
+        it('checks whether a property exists', function () {
             var memberNode = messageManagerControl.declarations[0].init.callee;
-            var value = astCheck.memberExpressionCheck(memberNode, "Cc", "@mozilla.org/childprocessmessagemanager;1");
+            var value = astCheck.memberExpressionCheck(memberNode, 'Cc', '@mozilla.org/childprocessmessagemanager;1');
             expect(value).to.equal(true);
         });
-        it("finds the identifier of the message manager", function () {
+        it('finds the identifier of the message manager', function () {
             var value = sinkFinder.findProcessMessageManager(messageManagerControl.declarations[0]);
             expect(value).to.equal('cpmm');
         })
-        it("finds the message passing functions", function () {
+        it('finds the message passing functions', function () {
             var value = sinkFinder.checkMessageFunction();
         })
     });
 });
 
-describe("AST node analysis", function () {
-    it("finds a specific member access expression", function () {
+describe('AST node analysis', function () {
+    it('finds a specific member access expression', function () {
         var memberNode = messageManagerControl.declarations[0].init.arguments[0];
-        var value = astCheck.memberExpressionCheck(memberNode, "Ci", "nsISyncMessageSender");
+        var value = astCheck.memberExpressionCheck(memberNode, 'Ci', 'nsISyncMessageSender');
         expect(value).to.equal(true);
     })
 });
