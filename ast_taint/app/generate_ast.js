bd34c1ebdbd930d6731d6ca6d12f73a704eb1957
DBelf <dimitri.belfor@gmail.com>
2017-05-17 18:37:55 +0200

    can create a new folder and save the file there

--- ast_taint/app/generate_ast.js
+++ ast_taint/app/generate_ast.js
@@ -7,6 +7,8 @@ var fs = require('fs'),
 
 var GenerateAST = (function () {
     var _EXTENSION = '.js';
+    var _DEFAULT_DIRECTORY = 'tmp'
+    var _DEFAULT_FILENAME = 'tmp.js';
 
     var collectFiles = function (parentPath) {
         var filesInPath = [];
@@ -33,6 +35,48 @@ var GenerateAST = (function () {
         return filesInPath;
     }
 
+    var createDirectory = function (dirPath) {
+        var splitPath = dirPath.split('/');
+        for (var i = 0; i < splitPath.length; i++) {
+            if (!fs.existsSync(splitPath[i])) {
+                fs.mkdirSync(splitPath[i]);
+            }
+        }
+    }
+
+    var createDefault = function () {
+        createDirectory(_DEFAULT_DIRECTORY);
+        fs.writeFile(_DEFAULT_DIRECTORY + '/' + _DEFAULT_FILENAME, '', (err) => {
+            console.log(err);
+        });
+    }
+
+    //FIXME breaks on nested folders.
+    var createWithFilePath = function (destination) {
+        var fullPath = destination.split('/');
+        if (fullPath.length > 1) {
+            createDirectory(fullPath.slice(0, -1).join('/'));
+        }
+        fs.writeFile(destination, '', (err) => {
+            console.log(err);
+        });
+    }
+
+    var createFile = function (destination) {
+        if (!destination) {
+           createDefault();
+        } else if (~destination.indexOf('.js')) {//bitwise ~ makes it true or false
+            createWithFilePath(destination);
+        } else {
+            createWithFilePath(destination.concat(_DEFAULT_FILENAME));
+        }
+    }
+
+
+    var appendToFile = function (file, destination) {
+
+    }
+    
     return {
         collectFiles: collectFiles
     }
