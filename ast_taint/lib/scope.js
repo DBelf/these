eb914b7582614a2f10c555ba15377e528353a74b
DBelf <dimitri.belfor@gmail.com>
2017-05-26 10:48:16 +0200

    started switching the style to airbnb style format

--- ast_taint/lib/scope.js
+++ ast_taint/lib/scope.js
@@ -2,178 +2,170 @@
  * Created by dimitri on 18/05/2017.
  */
 
-var estraverse = require('estraverse'),
-    generateAST = require('./generate_ast'),
-    utils = require('./utils'),
-    sourceFinder = require('./source_finder'),
-    escope = require('escope');
+const generateAST = require('./generate_ast');
+const utils = require('./utils');
+const sourceFinder = require('./source_finder');
+const escope = require('escope');
 
-var Scope = function () {
-
-    var TEMPLATE_SCOPE_MANAGER_WRAPPER = function (scope) {
-        return {
-            scope: scope,
-            knownSources: [],
-            knownSinks: []
-        }
-    }
-
-    var sourcesInGlobalScope = function (ast) {
-        var scopeManager = createScope(ast);
-        currentScope = scopeManager.acquire(ast);
-        return sourcesInScope(currentScope);
-    }
-
-    var sourcesInFile = function (ast) {
-        var scopeManager = createScope(ast);
-        var scopes = scopeManager.scopes;
-        var sources = [];
-        for (var i = 0; i < scopeManager.scopes.length; i++) {
-            var foundSources = sourcesInScope(scopes[i]);
-            sources = sources.concat(foundSources);
-        }
-        return sources.filter(n => n !== '');
-    }
-
-    var sourcesInScope = function (scope) {
-        var sources = []
-        var variablesInScope = scope.variables;
-        for (var i = 0; i < variablesInScope.length; i++) {
-            var defs = variablesInScope[i];
-            if (defs.name === 'arguments') {
-                continue;
-            }
-            var definedSource = checkDefsForSources(defs.defs);
-            sources = sources.concat(definedSource);
-
-        }
-
-        return sources.filter(n => n !== '');
-    }
-
-    //Dispatch and switch on the type???
-    var checkDefsForSources = function (definition) {
-        var defNode = definition[0].node;
-        if (defNode.type === 'VariableDeclarator') {
-            return sourceId(defNode);
-        }
-        return '';
-    }
-
-    //Returns the identifier of a source, or nothing if the node isn't a source.
-    var sourceId = function (node) {
-        return sourceFinder.checkDeclaration(node) ? node.id.name : '';
-    }
-
-    var pointsToInScope = function (identifier, scope) {
-        var scopeBody = getScopeBody(scope);
-        return pointsToInScopeBody(identifier, scopeBody);
-    }
-
-    var getScopeBody = function (scope) {
-        switch (scope.type) {
-            case 'global':
-                return scope.block.body;
-            case 'function':
-                return scope.block.body.body;
-            default:
-                return;
-                break;
-        }
-    }
-
-    //Not sure whether this works for nested forloops.
-    var pointsToInScopeBody = function (identifier, scopeBody) {
-        var uses = [];
-        for (var i = 0; i < scopeBody.length; i++) {
-            switch (scopeBody[i].type) {
-                case 'VariableDeclaration':
-                    uses = uses.concat(declarationAlias(scopeBody[i], identifier));
-                    break;
-                case 'ExpressionStatement':
-                    uses = uses.concat(expressionAlias(scopeBody[i], identifier));
-                    break;
-                default:
-                    break;
-            }
-        }
-        return uses.filter(n => n !== '');
-    }
-
-    var expressionAlias = function (node, identifier) {
-        var expression = node.expression;
-
-        if (utils.assignmentPointsTo(expression, identifier)) {
-            return expression.left.name;
-        }
-        return '';
-    }
-
-    var declarationAlias = function (node, identifier) {
-        var declarations = node.declarations;
-        var aliases = [];
-        for (var i = 0; i < declarations.length; i++) {
-            if (declarations[i].init !== null) {
-                if (utils.declarationPointsTo(declarations[i], identifier)) {
-                    aliases.push(declarations[i].id.name);
-                }
-            }
-        }
-        return aliases;
-    }
-
-    var findAllAliases = function (sourceArr, scope) {
-        var aliases = [];
-        for (var i = 0; i < sourceArr.length; i++) {
-            //This might miss the assign of a reassigned value.. But that would just be plain silly?
-            aliases = aliases.concat(pointsToInScope(sourceArr[i], scope));
-        }
-        return aliases;
-    }
-
-    var findReturnsInScope = function (scope) {
-        var returnStatements = [];
-        var scopeBody = getScopeBody(scope);
-        for (var i = 1; i < scopeBody.length; i++) {
-            switch (scopeBody[i].type){
-                case 'ReturnStatement':
-                    returnStatements = returnStatements.concat(scopeBody[i]);
-            }
-        }
-        return returnStatements;
-    }
-
-    var functionReturnsSource = function (scope) {
-        var sourcesInFunction = sourcesInScope(scope);
-        var returnsInFunction = findReturnsInScope(scope);
-        sourcesInFunction = sourcesInFunction.concat(findAllAliases(sourcesInFunction, scope));
-        var returnsSource = returnsInFunction.map(function (returnStatement) {
-            for (var i = 0; i < sourcesInFunction.length; i++){
-                if (utils.identifierUsedInReturn(sourcesInFunction[i], returnStatement)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-        return returnsSource.reduce(utils.reduceBoolean, false);
-    }
-
-    var createScope = function (ast) {
-        var scopeManager = escope.analyze(ast);
-        return scopeManager;
-    }
-
-    var ast = generateAST.astFromFile('../test/ast_tests/function_return.js');
-    var currentScope = createScope(ast).scopes[1];
-    var sources = sourcesInFile(ast);
-    console.log(functionReturnsSource(currentScope));
-
+const Scope = (function () {
+  const TEMPLATE_SCOPE_MANAGER_WRAPPER = function (scope) {
     return {
-        sourcesInGlobalScope: sourcesInGlobalScope,
-        sourcesInFile: sourcesInFile,
-        createScope: createScope
-    }
-}();
+      scope,
+      knownSources: [],
+      knownSinks: [],
+    };
+  };
 
-//TODO Recursive check over all child scopes??
+  const sourcesInGlobalScope = function (ast) {
+    const scopeManager = this.createScope(ast);
+    const currentScope = scopeManager.acquire(ast);
+    return this.sourcesInScope(currentScope);
+  };
+
+  const sourcesInFile = function (ast) {
+    const scopeManager = createScope(ast);
+    const scopes = scopeManager.scopes;
+    let sources = [];
+    for (let i = 0; i < scopeManager.scopes.length; i++) {
+      const foundSources = sourcesInScope(scopes[i]);
+      sources = sources.concat(foundSources);
+    }
+    return sources.filter(n => n !== '');
+  };
+
+  const sourcesInScope = function (scope) {
+    let sources = [];
+    const variablesInScope = scope.variables;
+    for (let i = 0; i < variablesInScope.length; i++) {
+      const defs = variablesInScope[i];
+      if (defs.name !== 'arguments') {
+        const definedSource = checkDefsForSources(defs.defs);
+        sources = sources.concat(definedSource);
+      }
+    }
+
+    return sources.filter(n => n !== '');
+  };
+
+    // Dispatch and switch on the type???
+  const checkDefsForSources = function (definition) {
+    const defNode = definition[0].node;
+    if (defNode.type === 'VariableDeclarator') {
+      return sourceId(defNode);
+    }
+    return '';
+  };
+
+    // Returns the identifier of a source, or nothing if the node isn't a source.
+  const sourceId = function (node) {
+    return sourceFinder.checkDeclaration(node) ? node.id.name : '';
+  };
+
+  const pointsToInScope = function (identifier, scope) {
+    const scopeBody = getScopeBody(scope);
+    return pointsToInScopeBody(identifier, scopeBody);
+  };
+
+  const getScopeBody = function (scope) {
+    switch (scope.type) {
+      case 'function':
+        return scope.block.body.body;
+      default:
+        return scope.block.body;
+    }
+  };
+
+    // Not sure whether this works for nested forloops.
+  const pointsToInScopeBody = function (identifier, scopeBody) {
+    let uses = [];
+    for (let i = 0; i < scopeBody.length; i++) {
+      switch (scopeBody[i].type) {
+        case 'VariableDeclaration':
+          uses = uses.concat(declarationAlias(scopeBody[i], identifier));
+          break;
+        case 'ExpressionStatement':
+          uses = uses.concat(expressionAlias(scopeBody[i], identifier));
+          break;
+        default:
+          break;
+      }
+    }
+    return uses.filter(n => n !== '');
+  };
+
+  const expressionAlias = function (node, identifier) {
+    const expression = node.expression;
+
+    if (utils.assignmentPointsTo(expression, identifier)) {
+      return expression.left.name;
+    }
+    return '';
+  };
+
+  const declarationAlias = function (node, identifier) {
+    const declarations = node.declarations;
+    const aliases = [];
+    declarations.forEach((declaration) => {
+      if (declaration !== null) {
+        if (utils.declarationPointsTo(declaration, identifier)) {
+          aliases.push(declaration.id.name);
+        }
+      }
+    });
+    return aliases;
+  };
+
+  const findAllAliases = function (sourceArr, scope) {
+    let aliases = [];
+    sourceArr.forEach((alias) => {
+      aliases = alias.concat(pointsToInScope(alias, scope));
+    });
+    return aliases;
+  };
+
+  const findReturnsInScope = function (scope) {
+    let returnStatements = [];
+    const scopeBody = getScopeBody(scope);
+    for (let i = 1; i < scopeBody.length; i++) {
+      switch (scopeBody[i].type) {
+        case 'ReturnStatement':
+          returnStatements = returnStatements.concat(scopeBody[i]);
+      }
+    }
+    return returnStatements;
+  };
+
+  const functionReturnsSource = function (scope) {
+    let sourcesInFunction = sourcesInScope(scope);
+    const returnsInFunction = findReturnsInScope(scope);
+    sourcesInFunction = sourcesInFunction.concat(findAllAliases(sourcesInFunction, scope));
+    const returnsSource = returnsInFunction.map((returnStatement) => {
+      for (let i = 0; i < sourcesInFunction.length; i++) {
+        if (utils.identifierUsedInReturn(sourcesInFunction[i], returnStatement)) {
+          return true;
+        }
+      }
+      return false;
+    });
+    return returnsSource.reduce(utils.reduceBoolean, false);
+  };
+
+  var createScope = function (ast) {
+    const scopeManager = escope.analyze(ast);
+    return scopeManager;
+  };
+
+  const ast = generateAST.astFromFile('../test/ast_tests/function_return.js');
+  const currentScope = createScope(ast).scopes[1];
+  const sources = sourcesInFile(ast);
+  console.log(functionReturnsSource(currentScope));
+
+  return {
+    sourcesInGlobalScope,
+    sourcesInFile,
+    createScope,
+  };
+}());
+
+// TODO Recursive check over all child scopes??
 module.exports = Scope;
