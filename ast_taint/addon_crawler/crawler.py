8a60a5cdbd8ea4bc14b03038ced37d62e3367e04
DBelf <dimitri.belfor@gmail.com>
2017-06-02 12:40:23 +0200

    can find all the xpi hrefs on a page and print them

--- ast_taint/addon_crawler/crawler.py
+++ ast_taint/addon_crawler/crawler.py
@@ -1,12 +1,18 @@
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+driver.get("https://addons.mozilla.org/nl/firefox/extensions/?sort=users&page=2")
+descriptionElementXPath = "//div[contains(@class, 'info')]";
+
+assert "Add-ons" in driver.title
+
+elems = driver.find_elements_by_xpath("//a[@href]")
+addons_in_elems = filter(lambda elem: ".xpi" in elem.get_attribute("href"), elems)
+for addon in addons_in_elems:
+    print addon.get_attribute("href")
+    
 assert "No results found." not in driver.page_source
 driver.close()
