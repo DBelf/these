6552ec68c47433e594c485d44bd4a1813789ea49
DBelf <dimitri.belfor@gmail.com>
2017-06-02 14:32:11 +0200

    Made the crawler

--- ast_taint/addon_crawler/crawler.py
+++ ast_taint/addon_crawler/crawler.py
@@ -1,7 +1,12 @@
-import re
+import urllib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+global DEFAULT_PATH = '../addons/'
+
+def download_url(addon_element):
+    filename = 'tmp';
+    urllib.urlretrieve(addon_element.get_attribute("href"), filename)
 
 driver = webdriver.Firefox()
 driver.get("https://addons.mozilla.org/nl/firefox/extensions/?sort=users&page=2")
@@ -10,9 +15,12 @@ descriptionElementXPath = "//div[contains(@class, 'info')]";
 assert "Add-ons" in driver.title
 
 elems = driver.find_elements_by_xpath("//a[@href]")
+
 addons_in_elems = filter(lambda elem: ".xpi" in elem.get_attribute("href"), elems)
-for addon in addons_in_elems:
-    print addon.get_attribute("href")
-    
+
+for addon_url in addons_in_elems:
+    download_url(addon_url)
+    break;
+
 assert "No results found." not in driver.page_source
 driver.close()
