695055428263c7f23839adf97eae2edcbb76d363
DBelf <dimitri.belfor@gmail.com>
2017-06-13 12:28:37 +0200

    can now read from the files

--- ast_taint/addon_crawler/filereader.py
+++ ast_taint/addon_crawler/filereader.py
@@ -1,14 +1,25 @@
 import itertools
 import subprocess
 import os
+import re
+import itertools
 
 BASEPATH = '../addons/'
 
-for folder in os.walk(BASEPATH).next()[1]:
-    subprocess.call('node ../lib/ASTTaint.js {0}'.format(BASEPATH + folder), shell=True)
+def parsevulnerability(line1, line2):
+    vulnerable_file = line1.split(',')[0].strip()
+    vulnerable_addon = line1.split(',')[0].split('/')[2].strip()
+    vulnerabilityType = line1.split(',')[1].strip()
+    match = re.search('{"start":{"line":(?P<start>\d*),"column":\d*},"end":{"line":(?P<end>\d*),"column":\d*}};', line2)
+    position = (int(match.group('start')), int(match.group('end')))
+    with open(vulnerable_file, "r") as text_file:
+        for line in itertools.islice(text_file, position[0] - 1, position[1]):
+            print line
 
 
-#
-# with open() as f:
-#     for line1,line2 in itertools.izip_longest(*[f]*2):
-#         print(line1,line2)
+# for folder in os.walk(BASEPATH).next()[1]:
+    # subprocess.call('node ../lib/ASTTaint.js {0}'.format(BASEPATH + folder), shell=True)
+
+with open('vulnerabilities/sources.txt') as f:
+    for line1,line2 in itertools.izip_longest(*[f]*2):
+        parsevulnerability(line1,line2)
